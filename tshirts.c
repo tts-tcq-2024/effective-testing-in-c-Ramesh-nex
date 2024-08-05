18eae6f2f213b68fc144fc88c2720b1b1c5a5d6a
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-08-05 11:26:13 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -18,7 +18,7 @@ int main() {
     assert(size(38) == 'M');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(42) == 'M');
+    assert(size(42) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
